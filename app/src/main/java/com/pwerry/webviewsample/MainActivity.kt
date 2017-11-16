7bd9d7be93cd21720383e82ba4b57379a8630877
peter <pwerry@gmail.com>
2017-11-16 10:08:22 -0800

    just open cnn.com

--- app/src/main/java/com/pwerry/webviewsample/MainActivity.kt
+++ app/src/main/java/com/pwerry/webviewsample/MainActivity.kt
@@ -2,11 +2,15 @@ package com.pwerry.webviewsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webview)
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://www.cnn.com")
     }
 }
